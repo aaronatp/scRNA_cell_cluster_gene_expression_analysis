67ce48661f94de5648a225ae8a657d4204b4f8a9
Aaronpresser <58194911+Aaronpresser@users.noreply.github.com>
2022-06-20 23:16:56 -0600

    Update lookup_gene.py

--- lookup_gene.py
+++ lookup_gene.py
@@ -36,13 +36,14 @@ for i in range(len(length)):
 
     
 class HiddenPrints:
+    '''Hides print statements of code called within it'''
     def __enter__(self):
         self._original_stdout = sys.stdout
         sys.stdout = open(os.devnull, 'w')
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         sys.stdout.close()
-        sys.stdout = self._original_stdout    
+        sys.stdout = self._original_stdout   
 
         
 def sorted_counter_dict(gene, data=data, proportion=True):
