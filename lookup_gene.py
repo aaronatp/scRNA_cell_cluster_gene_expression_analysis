30fc23811d2218bf29a24fbd2c6bf3e546eec433
Aaronpresser <58194911+Aaronpresser@users.noreply.github.com>
2022-06-24 20:19:34 -0600

    Update lookup_gene.py

--- lookup_gene.py
+++ lookup_gene.py
@@ -7,13 +7,13 @@ import sys, os
 from collections import Counter
 
 # Read in and preprocess data
-data = pd.read_csv('/Users/aaronpresser/files_from_quest/Filesfrom_Quest/Peng_RNA_seq/Gene_expression_analysis/seurat_integrated.csv')
-cells_to_clusters = pd.read_csv('/Users/aaronpresser/files_from_quest/Filesfrom_Quest/Peng_RNA_seq/Gene_expression_analysis/cells_to_clusters.csv')
+data = pd.read_csv('./Gene_expression_analysis/seurat_integrated.csv')
+cells_to_clusters = pd.read_csv('./Gene_expression_analysis/cells_to_clusters.csv')
 
 data.columns = ['Genes'] + list(cells_to_clusters['V1'])
 data.set_index("Genes", inplace=True)
 
-cell_idents = pd.read_csv('/Users/aaronpresser/files_from_quest/Filesfrom_Quest/Peng_RNA_seq/Gene_expression_analysis/integrated_cell_idents.csv')
+cell_idents = pd.read_csv('./Gene_expression_analysis/integrated_cell_idents.csv')
 cell_idents = list(cell_idents['V1'])
 cell_clusters = list(data.columns)
 new_cols = []
