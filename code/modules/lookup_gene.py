0750e64d8518e6bb0f55db5362d544626f20d144
Aaronpresser <58194911+Aaronpresser@users.noreply.github.com>
2022-09-06 17:31:46 -0500

    Update lookup_gene.py

--- code/modules/lookup_gene.py
+++ code/modules/lookup_gene.py
@@ -1,3 +1,6 @@
+# data is the raw gene expression counts data
+# cells_to_clusters tells you what cluster each cell belongs to
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,34 +8,6 @@ import matplotlib.pyplot as plt
 import sys, os
 
 from collections import Counter
-
-# Read in and preprocess data
-data = pd.read_csv('./Gene_expression_analysis/seurat_integrated.csv')
-cells_to_clusters = pd.read_csv('./Gene_expression_analysis/cells_to_clusters.csv')
-
-data.columns = ['Genes'] + list(cells_to_clusters['V1'])
-data.set_index("Genes", inplace=True)
-
-cell_idents = pd.read_csv('./Gene_expression_analysis/integrated_cell_idents.csv')
-cell_idents = list(cell_idents['V1'])
-cell_clusters = list(data.columns)
-new_cols = []
-for a,b in zip(data.columns, cell_idents):
-    a = str(a) + '_' + b
-    new_cols.append(a)
-
-data.columns = new_cols
-
-data_ctrl = data.filter(regex='CTRL$', axis=1)
-data_treat = data.filter(regex='TREAT$', axis=1)
-
-# Create dict with cell clusters as keys and number of cells in each as values
-data_dict = dict(sorted(dict(Counter(new_cols)).items()))
-length = set(key.split('_')[0] for key in data_dict.keys())
-cells_to_clusters['Cell_total'] = 0
-for i in range(len(length)):
-    cells_to_clusters['Cell_total'][str(i) + '_CTRL'] = data_dict[str(i) + '_CTRL']
-    cells_to_clusters['Cell_total'][str(i) + '_TREAT'] = data_dict[str(i) + '_TREAT']
 
 # Helper
 class HiddenPrints:
@@ -65,7 +40,7 @@ def sorted_counter_dict(gene, data=data, proportion=True):
     return {k: v for k, v in sorted(gene_dict.items(), key=lambda item: item[1], reverse=True)}
 
 # Helper
-def sort_dict_by_freq(store):
+def _sort_dict_by_freq(store):
     '''Sorts a dictionary's items by the frequency of the values in it'''
     freq = {} 
     for value in store.values(): 
@@ -79,54 +54,57 @@ def sort_dict_by_freq(store):
     store = dict(sorted(store.items(), key=lambda item: pos.index(item[1]))) 
     return store
 
+
+def _gene_expression_helper(gene, sure, df):
+    options = df.filter(like=f'{gene}', axis=0).index
+    if len(options) == 0:
+        return print("Try again")
+    elif len(options) > 1 and sure==False:
+        list_of_options = '\n'.join(i for i in options)
+        print(f"There are multiple genes that match your search:")
+        return print(list_of_options)
+    elif len(options) == 1 or sure==True:
+        sorted_dict = sorted_counter_dict(gene)
+        return sorted_dict
+    
+
+def _gene_list_printing_statement(sorted_cluster_significances):
+    most_common_clusters = list(sorted_cluster_significances.values()) 
+    first = max(set(most_common_clusters), key=most_common_clusters.count)
+    most_common_clusters = list(filter(lambda a: a != first, most_common_clusters))
+    second = max(set(most_common_clusters), key=most_common_clusters.count)
+    most_common_clusters = list(filter(lambda a: a != second, most_common_clusters))
+    third = max(set(most_common_clusters), key=most_common_clusters.count)
+    
+    print(f"The genes and their most common clusters are:\n{sorted_cluster_significances}"
+            f"The most common cluster is {first}, followed by {second}, followed by {third}")
+    
+    
+def _gene_list_helper(gene=gene, sure=sure, df=df):
+    counter==True and gene_list is not None:
+    cluster_significances = {}
+    for i in gene_list:
+        try:
+            cluster_significances[i] = list(sorted_counter_dict(gene=i).keys())[0]
+        except KeyError:
+            continue
+    
+    sorted_cluster_significances = _sort_dict_by_freq(cluster_significances)
+    return _gene_list_printing_statement(sorted_cluster_significances)
+
+
 # User
-def lookup_gene(gene=None, sure=False, gene_list=None, df=data, counter=False, raw=False):
+def lookup_gene(gene=None, sure=False, gene_list=None, df=data):
     """By default, will see whether input gene is unique,
         - if the input matches more than one gene in list, returns list of genes that match
         - if the input doesn't match any gene in list, tells the user to try again
         - if the input matches one gene in list, returns a pd.Series of clusters and expression levels
-        
-        If counter=True,
-        - returns a dict of clusters ordered by how many cells are in each"""
-    if gene is not None:
-        options = df.filter(like=f'{gene}', axis=0).index
-        if len(options) > 1 and sure==False:
-            list_of_options = '\n'.join(i for i in options)
-            print(f"There are multiple genes that match your search:")
-            return print(list_of_options)
-        if len(options) == 0:
-            return print("Try again")
-        if sure == True and raw == True:
-            return data.loc[f'{gene}'][data.loc[f'{gene}'] > 0]
-        if len(options) == 1 or sure==True:
-#             if proportion==True:
-            sorted_dict = sorted_counter_dict(gene)
-#             else:
-#                 ### Currently setting 'proportion == False' returns 'AttributeError: 'bool' object has no attribute 'loc'
-#                 sorted_dict = sorted_counter_dict(gene, proportion==False)
-#             if __name__ == 'main':
-#                 print(f"The cell clusters that express {gene}, sorted according to how many cells in the cluster express {gene}, are:")
-            return sorted_dict
+        """
+    if (gene is not None) and (gene_list is not None):
+        return print("Use either \"gene\" or \"gene_list\" but not both!")
     
-    if counter==False and gene_list is not None:
-        return print("In order to input a gene list, set 'counter == True'")
-    if counter==True and gene_list is not None:
-        cluster_significances = {}
-        for i in gene_list:
-            try:
-                cluster_significances[i] = list(sorted_counter_dict(gene=i).keys())[0]
-            except KeyError:
-                continue
-        cluster_significances = sort_dict_by_freq(cluster_significances)
-        
-        most_common_clusters = list(cluster_significances.values()) 
-        first = max(set(most_common_clusters), key=most_common_clusters.count)
-        most_common_clusters = list(filter(lambda a: a != first, most_common_clusters))
-        second = max(set(most_common_clusters), key=most_common_clusters.count)
-        most_common_clusters = list(filter(lambda a: a != second, most_common_clusters))
-        third = max(set(most_common_clusters), key=most_common_clusters.count)
-        
-        print(f"The genes and their most common clusters are:\n{cluster_significances}"
-                f"The most common cluster is {first}, followed by {second}, followed by {third}")
-        
-        return
+    elif gene is not None:
+        return _gene_expression_helper(gene=gene, sure=sure, df=df)
+    
+    elif gene_list is not None:
+        return _gene_list_helper(gene=gene, sure=sure, df=df)
