2a83e8552108943e0d8989f6efafc51efbbafd6d
Aaronpresser <58194911+Aaronpresser@users.noreply.github.com>
2022-09-06 16:02:20 -0500

    Update find_targets.py

--- code/modules/find_targets/find_targets.py
+++ code/modules/find_targets/find_targets.py
@@ -1,7 +1,16 @@
 import helpers
 import pandas as pd
 
+surface_genes = pd.read_csv('./Gene_expression_analysis/surface_genes.csv')
+surface_genes.rename(columns={'To': 'Genes'}, inplace=True)
+surface_genes.drop(columns=['Unnamed: 0', 'From'], inplace=True)  
+  
+brain_genes = pd.read_csv('./Downloads/genes_expressed_in_brain_proteinatlas.tsv', sep='\t')
+human_genes = pd.read_csv('./Downloads/human_genes_proteinatlas.tsv', sep='\t')
+
 synonyms = human_genes['Gene synonym'].apply(_synonym_cleaner)
+surfaceome_concat = ' '.join(i for i in surface_genes['Genes'])
+
 
 def genes_expressed(cluster, data=data_treat):
     '''Returns a list of all the genes recorded to be expressed in a particular cluster'''
